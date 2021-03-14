bd4319a95acf5cc52fe43eab88a9f4a5875c902d
Stiven Ramírez Arango <stivenramireza@gmail.com>
2021-03-14 11:23:18 -0500

    Adding book_date as parameter

--- smartfit_booking/formatter.py
+++ smartfit_booking/formatter.py
@@ -4,10 +4,7 @@ import time
 
 def get_current_date() -> str:
     return datetime.now().strftime("%d/%m/%Y")
-
-def search_current_date() -> str:
-    return datetime.now().strftime("%d%m%Y")
-
+    
 def convert_24_to_12_hour(time_value: str) -> str:
     t = time.strptime(time_value, "%H:%M")
     return time.strftime("%I:%M %p", t)
